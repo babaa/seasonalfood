76083243a1700151b9d53b41a0f6c4cc32e150ee
Jette Feigenbaum <jettefeigenbaum@gmail.com>
2014-04-12 18:26:00 +0300

    new table design

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,18 +3,4 @@
 
 $(document).ready(function(){
 
-  // Window height
-  var wheight = $(window).height() + 30 ;
-  $('.month').css('height', wheight);
-
-  // Scroll to current monthNames
-  var d = new Date();
-
-  var monthNames = [ "january", "february", "march", "april", "may", "june",
-    "july", "august", "september", "october", "november", "december" ];
-
-  var currentMonth = monthNames[d.getMonth()];
-
-  window.location.hash = currentMonth;
-
 })
